2dbb13ff71ba4120a849089273e277b1412f9e5d
github username <github email address>
2020-05-07 03:35:58 -0700

    working scroll and searchbox component, working build

---
+++ src/containers/App.jsx
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+import SearchBox from "../components/SearchBox";
+import CardList from "../components/CardList";
+import Scroll from "../components/Scroll";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      robots: [],
+      searchfield: "",
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ robots: users }));
+  }
+
+  onSearchChange = (event) => {
+    this.setState({ searchfield: event.target.value });
+  };
+
+  render() {
+    const { robots, searchfield } = this.state;
+    const filteredRobots = robots.filter((robot) => {
+      return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+    });
+    if (!robots.length) {
+      return <h1 className="tc">Loading...</h1>;
+    } else {
+      return (
+        <div className="tc">
+          <h1 className="f1">RoboDex</h1>
+          <SearchBox searchChange={this.onSearchChange} />
+          <Scroll>
+            <CardList robots={filteredRobots} />
+          </Scroll>
+        </div>
+      );
+    }
+  }
+}
+
+export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./containers/App";
 import "./styles/_index.scss";
 import "tachyons";
-import CardList from "./components/CardList";
-import { robots } from "./content/robots";
 
-ReactDOM.render(<CardList robots={robots} />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
