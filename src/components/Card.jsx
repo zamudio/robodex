bfb735192d927b04578fbb1191de340016411800
github username <github email address>
2020-05-07 14:49:53 -0700

    change to pokemon

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,7 +3,11 @@ import React from "react";
 const Card = ({ name, email, id }) => {
   return (
     <div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
-      <img alt="robot" src={`https://robohash.org/${id}?200x200`} />
+      {/* <img alt="robot" src={`https://robohash.org/${id}?200x200`} /> */}
+      <img
+        alt="robot"
+        src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`}
+      />
       <div>
         <h2>{name}</h2>
         <p>{email}</p>
