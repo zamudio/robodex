62b742d0a62c7fcf9bb9a7c669dee8336fb63168
github username <github email address>
2020-05-16 03:36:09 -0700

    Updates

---
+++ precache-manifest.b04cba22b4e2c9b83ea24b73aa94d538.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a796da685f7b72c5459dfdc01f5ce5a4",
+    "url": "/robodex/index.html"
+  },
+  {
+    "revision": "6891240ca279071f6b00",
+    "url": "/robodex/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "ce072ebf428da79e841d",
+    "url": "/robodex/static/css/main.f7b2decb.chunk.css"
+  },
+  {
+    "revision": "6891240ca279071f6b00",
+    "url": "/robodex/static/js/2.f3d8a2bc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/robodex/static/js/2.f3d8a2bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce072ebf428da79e841d",
+    "url": "/robodex/static/js/main.fd7f5607.chunk.js"
+  },
+  {
+    "revision": "ae291d6c0d22e2fd54cd",
+    "url": "/robodex/static/js/runtime-main.fb3d1593.js"
+  }
+]);
